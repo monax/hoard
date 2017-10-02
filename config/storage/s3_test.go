6751920d04e8525ea11384d568b264ab5ea1bd2e
Silas Davis <silas@erisindustries.com>
2017-10-02 19:25:34 +0100

    goimports

--- config/storage/s3_test.go
+++ config/storage/s3_test.go
@@ -1,13 +1,6 @@
 package storage
 
-import (
-	"testing"
-
-	"os"
-
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/stretchr/testify/assert"
-)
+import "testing"
 
 func TestDefaultS3Config(t *testing.T) {
 	assertStorageConfigSerialisation(t, DefaultS3Config())
