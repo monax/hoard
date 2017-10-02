a904307036f268f42bedf98cd2cc693942862b95
Silas Davis <silas@erisindustries.com>
2017-10-02 19:24:34 +0100

    Remove redundant test that may leak keys

--- config/storage/s3_test.go
+++ config/storage/s3_test.go
@@ -9,30 +9,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAWSCredentialsFromConfig(t *testing.T) {
-	chain := []*CredentialsProviderConfig{
-		{
-			Provider: EnvProviderName,
-		},
-	}
-
-	value := credentials.Value{
-		AccessKeyID:     "access-key-1",
-		SecretAccessKey: "secret-access-key-1",
-		ProviderName:    credentials.EnvProviderName,
-	}
-
-	err := os.Setenv("AWS_ACCESS_KEY_ID", value.AccessKeyID)
-	assert.NoError(t, err)
-	err = os.Setenv("AWS_SECRET_ACCESS_KEY", value.SecretAccessKey)
-	assert.NoError(t, err)
-	creds, err := AWSCredentialsFromChain(chain)
-	assert.NoError(t, err)
-	valueOut, err := creds.Get()
-	assert.NoError(t, err)
-	assert.Equal(t, value, valueOut)
-}
-
 func TestDefaultS3Config(t *testing.T) {
 	assertStorageConfigSerialisation(t, DefaultS3Config())
 }
