58c149e87a4e39a9034af1d9a01f60b799bc7112
Gregory Hill <greg.hill@monax.io>
2019-02-13 17:58:20 +0000

    ipfs integration test
    Signed-off-by: Gregory Hill <greg.hill@monax.io>

---
+++ integration/test_ipfs.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# Requires:
+# - Docker compose
+# - jq
+
+if [[ "$(which ipfs)" == "" ]]
+then
+  echo "Integration test requires IPFS"
+  exit 1
+fi
+
+# Integration test dir
+cd "$(dirname "$0")"
+
+read -d '' HOARD_JSON_CONFIG << CONFIG
+  {
+    "ListenAddress": "tcp://:53431",
+    "Storage": {
+      "StorageType": "ipfs",
+      "AddressEncoding": "base64",
+      "Protocol": "http://",
+      "Address": "localhost",
+      "Port": "5001"
+    },
+    "Logging": {
+      "LoggingType": "json",
+      "Channels": [
+        "info",
+        "trace"
+      ]
+    },
+    "Secrets": null
+  }
+CONFIG
+
+export HOARD_JSON_CONFIG
+echo "Running integration test with config:"
+echo ${HOARD_JSON_CONFIG} | jq '.'
+
+# Build unless asked not to
+echo "Bringing up integration test containers with docker-compose..."
+docker run -d --name=ipfs --network=host ipfs/go-ipfs:latest 
+# Make sure IPFS is configured
+sleep 5
+[ -z "$NOBUILD" ] && docker-compose build
+docker-compose up --build --exit-code-from hoarctl --abort-on-container-exit
